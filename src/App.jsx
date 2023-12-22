d51535b2408dc2573cabe542781239e8d705c7d2
Nouman Khan <nomiofficial68@gmail.com>
2023-12-22 15:21:55 +0500

    build runed

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
-import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
-import TicTacToe from './components/TicTacToe'
+import React from "react";
+import "./App.css";
+import TicTacToe from "./components/TicTacToe";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-   <TicTacToe/>
-  )
+  return <TicTacToe />;
 }
 
-export default App
+export default App;
